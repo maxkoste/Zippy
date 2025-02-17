5fa6549c0db12e533c11c6e94561c22b2a8b2119
Max Koste <maxkoste@gmail.com>
2025-02-17 20:45:27 +0100

    this code crashed my computer! going into debugg mode.....

--- src/hoffman.cpp
+++ src/hoffman.cpp
@@ -5,14 +5,59 @@
 #include <cctype>
 #include <vector>
 #include <algorithm>
+#include <queue>
 
 using namespace std;
+
+struct Node{
+
+    char data;
+    int frequency;
+    Node* left;
+    Node* right;
+    Node(char d, int f) : data(d), frequency(f), left(nullptr), right(nullptr){}
+
+    bool operator>(const Node& other) const {
+        return frequency > other.frequency;
+    };
+};
+
 
 bool compareFrequency(const pair<char, int>& a, const pair<char, int>& b){
     return a.second > b.second;
 }
 
-void countLetterFrequency(const string& filePath){
+Node* buildHuffmanTree(map<char, int>& letterFrequency){
+    //creating minHeap a sorted priorety_queue
+    priority_queue<Node*, vector<Node*>, greater<Node*>> minHeap;
+
+    for(const auto& pair : letterFrequency){
+        minHeap.push(new Node(pair.first, pair.second));
+    }
+
+    int nodeCounter = 0; 
+
+    while(minHeap.size() > 1){
+
+        if(nodeCounter > 1000){
+            cout << "Too many nodes. Stopping early." << endl;
+        }
+
+        nodeCounter++;
+
+        Node* left = minHeap.top(); minHeap.pop();
+        Node* right = minHeap.top(); minHeap.pop();
+
+        Node* internalNode = new Node('\0', left->frequency + right->frequency);
+        internalNode->left = left;
+        internalNode->right = right;
+
+        minHeap.push(internalNode);
+    }
+    return minHeap.top();
+}
+
+map<char, int> countLetterFrequency(const string& filePath){
 
     ifstream f(filePath);
 
@@ -35,19 +80,36 @@ void countLetterFrequency(const string& filePath){
 
     f.close();
 
+    //sorting the map
     vector<pair<char, int>> sortedVec(letterFrequency.begin(), letterFrequency.end());
-
     sort(sortedVec.begin(), sortedVec.end(), compareFrequency);
 
     //print the amount of times the letter occurs to the terminal!
     for(const auto& pair : sortedVec){
         cout << pair.first << " " << pair.second << endl;
     }
+
+    return letterFrequency;
+}
+
+void deleteTree(Node* root){
+    if(root == nullptr){
+        return;
+    }
+    deleteTree(root->left);
+    deleteTree(root->right);
+    delete root;
 }
 
 int main(){
 
     string filePath = "../test.txt";
-    countLetterFrequency(filePath);
+
+    map<char, int> letterFrequency = countLetterFrequency(filePath);
+
+    Node* root = buildHuffmanTree(letterFrequency);
+
+    deleteTree(root);
+    
     return 0;
 }

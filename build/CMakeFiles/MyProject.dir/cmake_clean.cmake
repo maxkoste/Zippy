db8be6430e99c5e02b8b1fd7d0c73c47d26cd93e
Max Koste <maxkoste@gmail.com>
2025-02-17 17:39:25 +0100

    inital commit

---
+++ build/CMakeFiles/MyProject.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/Users/maxkoste/IdeaProjects/C_projects/Zippy/bin/MyProject"
+  "/Users/maxkoste/IdeaProjects/C_projects/Zippy/bin/MyProject.pdb"
+  "CMakeFiles/MyProject.dir/src/main.cpp.o"
+  "CMakeFiles/MyProject.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
